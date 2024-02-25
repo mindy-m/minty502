d15cde0ec1e2704f0a00137a3e25c713f8b75d5f
Mindy Medeiros <mmedei6@wgu.edu>
2024-02-25 05:25:29 -0500

    Added functions such as ora, cmp, and branch_if

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,25 +1,34 @@
 use super::*;
 
 impl Registers {
-    pub fn lda<AM: ReadableAddressingMode>(&mut self, memory: &mut Memory) {
+    pub(crate) fn lda<AM: ReadableAddressingMode>(
+        &mut self,
+        memory: &mut Memory,
+    ) {
         let addressing_mode = AM::new(self, memory);
         let value = addressing_mode.read(self, memory);
         self.a = self.status_nz(value);
     }
 
-    pub fn ldx<AM: ReadableAddressingMode>(&mut self, memory: &mut Memory) {
+    pub(crate) fn ldx<AM: ReadableAddressingMode>(
+        &mut self,
+        memory: &mut Memory,
+    ) {
         let addressing_mode = AM::new(self, memory);
         let value = addressing_mode.read(self, memory);
         self.x = self.status_nz(value);
     }
 
-    pub fn ldy<AM: ReadableAddressingMode>(&mut self, memory: &mut Memory) {
+    pub(crate) fn ldy<AM: ReadableAddressingMode>(
+        &mut self,
+        memory: &mut Memory,
+    ) {
         let addressing_mode = AM::new(self, memory);
         let value = addressing_mode.read(self, memory);
         self.y = self.status_nz(value);
     }
 
-    pub fn store<AM: WritableAddressingMode>(
+    pub(crate) fn store<AM: WritableAddressingMode>(
         &mut self,
         memory: &mut Memory,
         value: u8,
@@ -27,5 +36,41 @@ impl Registers {
         let addressing_mode = AM::new(self, memory);
         addressing_mode.write(self, memory, value);
     }
-    //
+
+    pub(crate) fn ora<AM: ReadableAddressingMode>(
+        &mut self,
+        memory: &mut Memory,
+    ) {
+        let addressing_mode = AM::new(self, memory);
+        let value = addressing_mode.read(self, memory);
+        self.a = self.status_nz(value | self.a);
+    }
+
+    pub(crate) fn cmp<AM: ReadableAddressingMode>(
+        &mut self,
+        memory: &mut Memory,
+    ) {
+        let addressing_mode = AM::new(self, memory);
+        let value = addressing_mode.read(self, memory);
+        // We don't care what the resulting value is, we just care about the
+        // flag bits getting set.
+        let _ = self.evil_add(!value, true);
+    }
+
+    //self.branch_if(memory, (self.flags & STATUS_C) == STATUS_C);
+    pub(crate) fn branch_if(&mut self, memory: &mut Memory, condition: bool) {
+        let branch_target = self.get_branch_target(memory);
+        if condition {
+            self.pc = branch_target;
+        }
+    }
+
+    // not public, too evil
+    fn evil_add(&mut self, value: u8, carry_in: bool) -> u8 {
+        let first_thing = self.a as u16;
+        let second_thing = value as u16;
+        let third_thing = if carry_in { 1 } else { 0 };
+        let resulting_thing = first_thing + second_thing + third_thing;
+        self.status_cnz(resulting_thing)
+    }
 }
